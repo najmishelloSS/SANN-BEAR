92712e6c29b5976cc77e2dfd5a66abe07eafa3b2
Alipzdn <aliffaizuddin47@gmail.com>
2023-12-01 01:39:35 +0800

    Room Registration (1)

--- ktdi_app/src/app/room-registration/room-registration.page.ts
+++ ktdi_app/src/app/room-registration/room-registration.page.ts
@@ -9,7 +9,30 @@ export class RoomRegistrationPage implements OnInit {
 
   constructor() { }
 
+  bookingModal = false; 
+  selectedSegment: string = 'default';
+
+  setOpen(isOpen: boolean, modalName: String) // open or close modal
+  {
+    if(modalName == "bookingModal"){
+      this.bookingModal = isOpen; 
+    }
+
+  }
+
+  continueClicked() {
+    // Access the selected value from the ion-segment
+    console.log('Selected Segment:', this.selectedSegment)
+    // Further logic based on the selected value
+    // For example, navigate to another page or perform an action
+  }
+
+  BookRoom (){
+    
+  }
+
   ngOnInit() {
   }
 
 }
+
