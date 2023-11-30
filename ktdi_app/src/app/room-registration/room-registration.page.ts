24265854f237e94849bd9a58bd8c379a77263940
Alipzdn <aliffaizuddin47@gmail.com>
2023-12-01 02:42:33 +0800

    Update

--- ktdi_app/src/app/room-registration/room-registration.page.ts
+++ ktdi_app/src/app/room-registration/room-registration.page.ts
@@ -10,25 +10,57 @@ export class RoomRegistrationPage implements OnInit {
   constructor() { }
 
   bookingModal = false; 
+
+  // double Room
+  doubleBlockModal = false;
+  doubleLevelModal = false;
+  doubleRoomModal = false;
+
+
+  // Single Room 
+  singleBlockModal = false;
+  singleLevelModal = false;
+  singleRoomModal = false;
+
   selectedSegment: string = 'default';
+  selectedLevel: string = 'default';
+  selectedRoom: string = 'default';
 
   setOpen(isOpen: boolean, modalName: String) // open or close modal
   {
     if(modalName == "bookingModal"){
       this.bookingModal = isOpen; 
     }
-
-  }
-
-  continueClicked() {
-    // Access the selected value from the ion-segment
-    console.log('Selected Segment:', this.selectedSegment)
-    // Further logic based on the selected value
-    // For example, navigate to another page or perform an action
-  }
-
-  BookRoom (){
+    else if(modalName == "doubleBlockModal"){
+      this.doubleBlockModal = isOpen; 
+    }
+    else if(modalName == "singleBlockModal"){
+      this.singleBlockModal = isOpen; 
+    }
+    else if(modalName == "singleLevelModal"){
+      this.singleLevelModal = isOpen; 
+    }
+    else if(modalName == "doubleLevelModal"){
+      this.singleBlockModal = isOpen; 
+    }
     
+    
+
+  }
+
+  selectBlock() {
+    // Access the selected value from the ion-segment
+    this.setOpen(true, this.selectedSegment);
+  }
+
+  selectLevel() {
+    // Access the selected value from the ion-segment
+    this.setOpen(true, this.selectedLevel);
+  }
+
+  selectRoom() {
+    // Access the selected value from the ion-segment
+    this.setOpen(true, this.selectedSegment);
   }
 
   ngOnInit() {
