2b871adbbe650b1bd2426ca169bf66b6074a11e5
Alipzdn <aliffaizuddin47@gmail.com>
2023-12-06 23:19:40 +0800

    update

--- ktdi_app/src/app/room-registration/room-registration.page.ts
+++ ktdi_app/src/app/room-registration/room-registration.page.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./room-registration.page.scss'],
 })
 
+//new coding
 
 
 export class RoomRegistrationPage implements OnInit {
@@ -15,50 +16,35 @@ export class RoomRegistrationPage implements OnInit {
 
   
 
-  blockModal = false; 
-  roomModal = false; 
-  displayRoomModal = false; 
+  roomTypeModal = false; 
+  availableBlockModal = false; 
 
-
+  selectRoomType: string = 'default'; 
+  selectedBlock: string = 'default'; 
+  generatedRoomNumber: string = '';
 
   setOpen(isOpen: boolean, modalName: String) // open or close modal
   { 
-    if(modalName == "blockModal"){
-      this.blockModal = isOpen; 
+    if(modalName == "roomTypeModal"){
+      this.roomTypeModal = isOpen; 
     }
-    else if (modalName == "roomModal")
+    else if (modalName == "availableBlockModal")
     {
-      this.roomModal = isOpen; 
-    }
-    else if (modalName == 'displayRoomModal')
-    {
-      this.displayRoomModal = isOpen;
+      this.availableBlockModal = isOpen; 
     }
 
   }
 
-  selectBlockType () 
+  selectedRoomType () 
   {
-    this.setOpen(true, 'roomModal');
+    this.setOpen(true, 'availableBlockModal');
   }
+
+
 
   // Function to generate a random floor and room number
-generateRandomRoomNumber(): string {
-  const floors = ['G', '1', '2', '3'];
-  const randomFloor = floors[Math.floor(Math.random() * floors.length)];
-  const randomRoom = Math.floor(Math.random() * 60) + 1; // Generates a random number between 1 and 60
-  return `${randomFloor}${randomRoom}`;
-}
 
-  confirmClicked() {
-    // Generate a random room number
-    this.generatedRoomNumber = this.generateRandomRoomNumber();
-  
-    // Open the modal to display the room number
-    this.setOpen(true, 'displayRoomModal');
-  }
-
-
+ 
 
   ngOnInit() {
   }
