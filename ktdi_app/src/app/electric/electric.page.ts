862ba1d6101f342cc0c601287ba0d9f611e14e34
nabil-01-utm <nabil-01@graduate.utm.my>
2023-12-07 01:46:29 +0800

    electric update
    pic added

--- ktdi_app/src/app/electric/electric.page.ts
+++ ktdi_app/src/app/electric/electric.page.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-electric',
   templateUrl: './electric.page.html',
   styleUrls: ['./electric.page.scss'],
 })
-export class ElectricPage implements OnInit {
+export class ElectricPage {
 
-  constructor() { }
+  constructor(private navCtrl: NavController) {}
 
-  ngOnInit() {
+  selectAppliance(appliance: string) {
+    // Handle the selection of appliance, you can add logic to register or navigate to a detailed registration page
+    console.log('Selected Appliance:', appliance);
+    // Example: Navigating to a detailed registration page with selected appliance
+    this.navCtrl.navigateForward(`/appliance-registration/${appliance}`);
   }
-
 }
