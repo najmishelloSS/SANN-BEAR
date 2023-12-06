323db4e3710c9ddbf1dae6f928e49111fbbb9308
Abdul Barik <129040507+Abd-Barik@users.noreply.github.com>
2023-12-07 00:43:20 +0800

    Home Page/Searchbar

--- ktdi_app/src/app/home/home.module.ts
+++ ktdi_app/src/app/home/home.module.ts
@@ -5,6 +5,9 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
+import { SearchbarComponent } from '../tool/searchbar/searchbar.component';
+import { SearchbarModule } from '../tool/searchbar/searchbar.module';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 
 @NgModule({
@@ -12,8 +15,10 @@ import { HomePageRoutingModule } from './home-routing.module';
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    SearchbarModule,
   ],
-  declarations: [HomePage]
+  declarations: [HomePage],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class HomePageModule {}
