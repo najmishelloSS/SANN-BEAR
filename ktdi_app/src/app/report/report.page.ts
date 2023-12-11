f790441c4b67057d607be9f0d27b005752116de6
Muhammad Najmi Zulhusni <93716543+najmishelloSS@users.noreply.github.com>
2023-12-11 22:49:53 +0800

    Update Report Status

---
+++ ktdi_app/src/app/report/report.page.ts
@@ -0,0 +1,50 @@
+// report.page.ts
+
+import { Component, OnInit } from '@angular/core';
+import { NavController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-report',
+  templateUrl: './report.page.html',
+  styleUrls: ['./report.page.scss'],
+})
+export class ReportPage implements OnInit {
+
+  damageTypes: { label: string; checked: boolean }[] = [
+    { label: 'Pipe Leakage', checked: false },
+    { label: 'Fan', checked: false },
+    { label: 'Lamp', checked: false },
+    { label: 'Clogged Sink', checked: false },
+    { label: 'Wall Plug', checked: false },
+  ];
+
+  damageDescription: string = '';
+  submitted: boolean = false;
+
+  constructor(private navCtrl: NavController) { }
+
+  ngOnInit() {
+  }
+
+  submitReport() {
+    // Add logic to handle the submission of the report
+    console.log('Submitting Report');
+
+    // Set submitted to true to show the confirmation message
+    this.submitted = true;
+
+    // Reset the form data
+    this.resetForm();
+  }
+
+  resetForm() {
+    // Reset form data
+    this.damageTypes.forEach(type => type.checked = false);
+    this.damageDescription = '';
+  }
+
+  backToHome() {
+    // Navigate back to home
+    this.navCtrl.navigateRoot('/home');
+  }
+}
