9151f13a386f7b0715a965c881c8a8fc227aba32
Alipzdn <aliffaizuddin47@gmail.com>
2023-12-11 22:47:30 +0800

    Room Registration

--- ktdi_app/src/app/room-registration/room-registration.page.ts
+++ ktdi_app/src/app/room-registration/room-registration.page.ts
@@ -1,4 +1,7 @@
+//////////////DEPENDENCIES///////////////////////
 import { Component, OnInit } from '@angular/core';
+import { ComponentsService } from '../service/components.service';
+//////////////DEPENDENCIES///////////////////////
 
 @Component({
   selector: 'app-room-registration',
@@ -6,45 +9,110 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./room-registration.page.scss'],
 })
 
-//new coding
-
 
 export class RoomRegistrationPage implements OnInit {
 
-  constructor() { 
+  constructor(
+    public component: ComponentsService,
+  ) 
+  { 
   }
 
-  
+  async navigateModal(location, destination) { //close old modal and open new modal
+    this.setOpen(false, location) //close old modal
+    const loading = await this.component.loadingController.create({ //generate loading interface
+      message: "Please wait"
+    });
+    loading.present();
+    setTimeout(async ()=> { //delay action for 1 second
+      this.setOpen(true, destination) //open new modal
+      loading.dismiss(); //close loading interface
+    }, 1000);
+  }
 
-  roomTypeModal = false; 
-  availableBlockModal = false; 
+
+  roomTypeModal = true; 
+
+  // *** Single Room Component ****
+  availableBlockModalSingle = false; 
+  availableLevelModalSingle = false;
+  availableRoomModalSingle = false;
+  displayRoomModalSingle = false; 
+
 
   selectRoomType: string = 'default'; 
   selectedBlock: string = 'default'; 
-  generatedRoomNumber: string = '';
+  selectedLevel: string = 'default'; 
+  selectedRoom: string = 'default'; 
+  
 
   setOpen(isOpen: boolean, modalName: String) // open or close modal
   { 
     if(modalName == "roomTypeModal"){
       this.roomTypeModal = isOpen; 
     }
-    else if (modalName == "availableBlockModal")
-    {
-      this.availableBlockModal = isOpen; 
-    }
 
+    // **** Single Room Component ****
+    else if (modalName == "availableBlockModalSingle")
+    {
+      this.availableBlockModalSingle = isOpen; 
+    }
+    else if (modalName == "availableBlockModalSingle")
+    {
+      this.availableBlockModalSingle = isOpen; 
+    }
+    else if (modalName == "availableLevelModalSingle")
+    {
+      this.availableLevelModalSingle = isOpen; 
+    }
+    else if (modalName == "availableRoomModalSingle")
+    {
+      this.availableRoomModalSingle = isOpen; 
+    }
+    else if (modalName == "displayRoomModalSingle")
+    {
+      this.displayRoomModalSingle = isOpen; 
+    }
   }
+
 
   selectedRoomType () 
   {
-    this.setOpen(true, 'availableBlockModal');
+    this.setOpen(false, 'roomTypeModal')
+    this.setOpen(true, 'availableBlockModalSingle');
+  }
+
+  selectedBlockSingleType () 
+  {
+    this.setOpen(false, 'availableBlockModalSingle');
+    this.setOpen(true, 'availableLevelModalSingle');
+  }
+
+  selectedLevelSingleType () 
+  {
+    this.setOpen(false, 'availableLevelModalSingle');
+    this.setOpen(true, 'availableRoomModalSingle');
+  }
+
+  selectedRoomSingleType () 
+  {
+    this.setOpen(false, 'availableRoomModalSingle');
+    this.setOpen(true, 'displayRoomModalSingle');
   }
 
 
+  availableRooms = ['101', '102', '103', '104', '105']; // Example room numbers
 
-  // Function to generate a random floor and room number
+  selectRoom(roomNumber: string) {
+    // Implement your logic when a room is selected
+    console.log(`Room ${roomNumber} selected`);
+  }
 
- 
+  // Function to check if a room is selected
+isRoomSelected(roomNumber: string): boolean {
+  return this.selectedRoom === roomNumber;
+}
+
 
   ngOnInit() {
   }
