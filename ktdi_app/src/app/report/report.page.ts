7bec131336d643b4ec009de0626bc10edd59216e
Muhammad Najmi Zulhusni <93716543+najmishelloSS@users.noreply.github.com>
2023-12-12 09:47:58 +0800

    Status (database tak dapat link)

--- ktdi_app/src/app/report/report.page.ts
+++ ktdi_app/src/app/report/report.page.ts
@@ -1,14 +1,13 @@
-// report.page.ts
-
-import { Component, OnInit } from '@angular/core';
-import { NavController } from '@ionic/angular';
+import { Component } from '@angular/core';
+import { NavController, LoadingController, ToastController } from '@ionic/angular';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-report',
   templateUrl: './report.page.html',
   styleUrls: ['./report.page.scss'],
 })
-export class ReportPage implements OnInit {
+export class ReportPage {
 
   damageTypes: { label: string; checked: boolean }[] = [
     { label: 'Pipe Leakage', checked: false },
@@ -16,40 +15,73 @@ export class ReportPage implements OnInit {
     { label: 'Lamp', checked: false },
     { label: 'Clogged Sink', checked: false },
     { label: 'Wall Plug', checked: false },
+    { label: 'Other', checked: false },
   ];
 
   damageDescription: string = '';
   submitted: boolean = false;
 
-  constructor(private navCtrl: NavController) { }
+  constructor(
+    private navCtrl: NavController,
+    private loadingController: LoadingController,
+    private toastController: ToastController,
+    private http: HttpClient
+  ) { }
 
-  ngOnInit() {
-  }
-
-  chooseFile() {
-    // Add your logic for choosing a file here
-    console.log('File has been chosen');
-  }
-
-  submitReport() {
-    // Add logic to handle the submission of the report
+  async submitReport() {
     console.log('Submitting Report');
 
-    // Set submitted to true to show the confirmation message
-    this.submitted = true;
+    const reportData = {
+      file_path: 'path_to_uploaded_file', // Replace with the actual path
+      damage_type: this.getSelectedDamageTypes(),
+      damage_description: this.damageDescription,
+      user_email: 'user@example.com', // Replace with the actual user email
+    };
 
-    // Reset the form data
-    this.resetForm();
+    console.log('Report Data:', reportData);
+
+    const loading = await this.loadingController.create({
+      message: 'Please Wait...',
+    });
+    loading.present();
+
+    this.http.post<any>('http://ktdiapp.mooo.com/api/submit_report.php', reportData)
+      .subscribe(
+        (response) => {
+          console.log('Report submitted successfully:', response);
+          this.submitted = true;
+          this.resetForm();
+          loading.dismiss();
+        },
+        (error) => {
+          console.error('Error submitting report:', error);
+          loading.dismiss();
+          this.presentToast('Something went wrong, please try again later');
+        }
+      );
+  }
+
+  private getSelectedDamageTypes(): string[] {
+    return this.damageTypes
+      .filter(type => type.checked)
+      .map(type => type.label);
   }
 
   resetForm() {
-    // Reset form data
     this.damageTypes.forEach(type => type.checked = false);
     this.damageDescription = '';
   }
 
   backToHome() {
-    // Navigate back to home
     this.navCtrl.navigateRoot('/home');
+  }
+
+  async presentToast(message: string) {
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 3000,
+      position: 'bottom'
+    });
+    toast.present();
   }
 }
