94e3413c77ea7e7c4ad83ecd9815f25de92bcbe8
Abdul Barik <129040507+Abd-Barik@users.noreply.github.com>
2024-01-08 22:57:22 +0800

    8/1

--- ktdi_app/src/app/admin/admin.page.ts
+++ ktdi_app/src/app/admin/admin.page.ts
@@ -1,9 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-<<<<<<< Updated upstream
-=======
-import { NavController } from '@ionic/angular';
-
->>>>>>> Stashed changes
 
 @Component({
   selector: 'app-admin',
@@ -11,31 +6,14 @@ import { NavController } from '@ionic/angular';
   styleUrls: ['./admin.page.scss'],
 })
 export class AdminPage implements OnInit {
-<<<<<<< Updated upstream
 
   constructor() { }
-=======
-  constructor(private navCtrl: NavController) { }  // Inject NavController
->>>>>>> Stashed changes
 
   ngOnInit() {
   }
 
-<<<<<<< Updated upstream
   navigate(route, direction){
     console.log("Go " + direction + " to " +route )
-=======
-  navigate(route, direction) {
-    console.log("Go " + direction + " to " + route);
-
-    // Implement navigation logic
-    if (direction === 'forward') {
-      this.navCtrl.navigateForward(route);
-    } else if (direction === 'back') {
-      this.navCtrl.navigateBack(route);
-    }
-    // Add more conditions if needed
->>>>>>> Stashed changes
   }
 
 }
