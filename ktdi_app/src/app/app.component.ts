79e195cc7e2bdbae66ec5023fe57db4fc5c92657
Alipzdn <aliffaizuddin47@gmail.com>
2024-01-12 23:21:49 +0800

    update homepage
    new

--- ktdi_app/src/app/app.component.ts
+++ ktdi_app/src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
  import { Stripe } from '@capacitor-community/stripe';
+ import { register } from 'swiper/element/bundle';
+
+ register();
 
  @Component({
    selector: 'app-root',
