0038ca8ec229990c2bd1c16dd0c2ee7e633c0f67
Abdul Barik <129040507+Abd-Barik@users.noreply.github.com>
2024-01-14 23:36:16 +0800

    update 14/1 11:36pm

---
+++ ktdi_app/src/app/room-details/room-details.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-room-details',
+  templateUrl: './room-details.page.html',
+  styleUrls: ['./room-details.page.scss'],
+})
+export class RoomDetailsPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
