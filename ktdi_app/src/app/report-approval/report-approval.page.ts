12f821bd54da6e66d64566591d2f442471760d2c
Abdul Barik <129040507+Abd-Barik@users.noreply.github.com>
2024-01-14 18:44:31 +0800

    update 14/1

--- ktdi_app/src/app/report-approval/report-approval.page.ts
+++ ktdi_app/src/app/report-approval/report-approval.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-report-approval',
@@ -7,9 +8,63 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReportApprovalPage implements OnInit {
 
-  constructor() { }
+  selectedStatus: string = 'active';
+  activeSectionVisible: boolean = true;
+  successSectionVisible: boolean = false;
+  reportData: any[] = [];
+
+  constructor(private http: HttpClient) {}
 
   ngOnInit() {
+    this.fetchReportData();
+  }
+
+  onStatusChange() {
+    this.activeSectionVisible = this.selectedStatus === 'active';
+    this.successSectionVisible = this.selectedStatus === 'success';
+    if (this.selectedStatus === 'active') {
+      // Fetch all reports and reset the status to "Submitted"
+      this.fetchReportData();
+    } else {
+      // Fetch only the reports with "Success" status
+      this.fetchReportData(this.selectedStatus);
+    }
+  }
+
+  fetchReportData(status?: string) {
+    const url = 'http://ktdiapp.mooo.com/api/submit-report.php';
+    const params = {};
+
+    this.http.post<any>(url, params)
+      .subscribe(
+        (data: any) => {
+          console.log('Received data:', data);
+          if (status) {
+            // Filter the reports based on the selected status
+            this.reportData = data.Reports
+              .filter(report => report.status === status)
+              .map((report: any) => {
+                report.damage_type = report.damage_type ? [report.damage_type] : [];
+                return report;
+              });
+          } else {
+            // Reset the status to "Submitted" for all reports
+            this.reportData = data.Reports.map((report: any) => {
+              report.damage_type = report.damage_type ? [report.damage_type] : [];
+              report.status = 'Submitted';
+              return report;
+            });
+          }
+          console.log(this.reportData);
+        },
+        async error => {
+          console.error('Error fetching report data:', error);
+        }
+      );
+  }
+
+  formatId(id: number): string {
+    return '#' + ('00000' + id).slice(-5);
   }
 
 }
