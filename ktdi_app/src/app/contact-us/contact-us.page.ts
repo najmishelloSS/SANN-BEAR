fa1513bffaec6af5ce5916434368d419cf087d35
Abdul Barik <129040507+Abd-Barik@users.noreply.github.com>
2024-01-16 02:26:02 +0800

    update contact us
    button contact us cannot connect from home page to contact us page

--- ktdi_app/src/app/contact-us/contact-us.page.ts
+++ ktdi_app/src/app/contact-us/contact-us.page.ts
@@ -2,23 +2,44 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 
+interface Contact {
+  name: string;
+  contact: string;
+  email: string;
+}
+
 @Component({
   selector: 'app-contact-us',
   templateUrl: './contact-us.page.html',
   styleUrls: ['./contact-us.page.scss'],
 })
 export class ContactUsPage implements OnInit {
+  contactList: any[] = [];
 
-  constructor(private router: Router, private http: HttpClient) { }
+  constructor(private router: Router, private http: HttpClient) {}
 
   ngOnInit() {
+    this.fetchContactData();
   }
 
   navigate(route: string, direction: string) {
-    console.log("Go " + direction + " to " + route);
-    
+    console.log('Go ' + direction + ' to ' + route);
     // Update this part to navigate to the specified route
     this.router.navigate([route]);
   }
 
+  fetchContactData() {
+    // Update the URL with the correct path to your PHP script
+    const url = 'http://ktdiapp.mooo.com/api/contact.php';
+
+    this.http.get<any>(url).subscribe(
+      (data: any) => {
+        this.contactList = data.contactList || [];
+        console.log('Contact List:', this.contactList);
+      },
+      (error) => {
+        console.error('Error:', error);
+      }
+    );
+  }
 }
